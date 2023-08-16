c013cc48d3d805bacd68ff986ed778cc06da6ee2
Jianwei Xie <xiejw0217+db@gmail.com>
2023-08-16 11:30:26 -0700

    [torch] updated the script to remove some clang flags.

--- dotfiles/tools/torch_config.sh
+++ dotfiles/tools/torch_config.sh
@@ -18,14 +18,25 @@ fi
 # prepare cmake
 #
 
-. ../pyenv/bin/activate && \
-  which python3 &&  \
-  pip3 install pyyaml typing_extensions && \
-  cmake \
-    -G Ninja \
-    -DUSE_MPS=ON \
-    -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_BUILD_TYPE:STRING=Release \
-    -DPYTHON_EXECUTABLE:PATH=`which python3` \
+. ../pyenv/bin/activate &&                                    \
+  which python3 &&                                            \
+  pip3 install pyyaml typing_extensions &&                    \
+  cmake                                                       \
+    -G Ninja                                                  \
+    -DUSE_MPS=ON                                              \
+    -DBUILD_PYTHON=OFF                                        \
+    -DBUILD_SHARED_LIBS=ON                                    \
+    -DCMAKE_BUILD_TYPE:STRING=Release                         \
+    -DPYTHON_EXECUTABLE:PATH=`which python3`                  \
     -DCMAKE_INSTALL_PREFIX:PATH=../install-`date +'%Y-%m-%d'` \
     ../src
+
+#
+# remove clang flags to avoid compilation error.
+#
+# As of 2023-08, caffe2 has some files failing this clang flag. This patch
+# basically removes all of them.
+sed -i -r 's/-Wno-missing-prototypes//g' build.ninja
+sed -i -r 's/-Wmissing-prototypes//g' build.ninja
+sed -i -r 's/-Wno-error=missing-prototypes//g' build.ninja
+sed -i -r 's/-Werror=missing-prototypes//g' build.ninja
