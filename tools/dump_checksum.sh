b89485957a2ebf7b07083d840f15efbe5deb9c6d
Jianwei Xie <xiejw0217+db@gmail.com>
2023-10-13 12:54:33 -0700

    [tools] improved dump checksum.

--- tools/dump_checksum.sh
+++ tools/dump_checksum.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+#
+# vim: ft=bash
 
-set -x
+# set -x
 
 DATE=`date +"%Y-%m-%d"`
-find -L . -type f -not -path '*/.*' | sort | sed -e "s/^.\///" | xargs -I {} shasum -a 256 "{}" | tee ${DATE}_checksum.txt
+TMPFILE=$(mktemp /tmp/${DATE}_checksum.txt.XXXXXX)
+find -L . -type f -not -path '*/.*' | sort | sed -e "s/^.\///" | xargs -I {} shasum -a 256 "{}" | tee ${TMPFILE}
+echo "writen to ${TMPFILE}" 1>&2
