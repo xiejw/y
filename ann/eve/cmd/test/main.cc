2a8c2b5107abdecb8a108045921265198a72f356
Jianwei Xie <xiejw0217+db@gmail.com>
2024-01-28 14:28:50 -0800

    [eve] added basic framework for eve::test.

--- ann/eve/cmd/test/main.cc
+++ ann/eve/cmd/test/main.cc
@@ -1,7 +1,61 @@
 #include <iostream>
 
+#include <string>
+#include <vector>
+
+struct SuiteBaseTest {
+    virtual char *TestBody( ) = 0;
+};
+
+struct Suite {
+    std::string    name;
+    SuiteBaseTest *func;
+};
+
+class SuiteUmbrella {
+  public:
+    static std::vector<Suite> suites;
+
+    void RunAllSuites( )
+    {
+        for ( auto &suite : suites ) {
+            std::cout << "Running: " << suite.name << "\n";
+            suite.func->TestBody( );
+        }
+    }
+};
+
+std::vector<Suite> SuiteUmbrella::suites{ };
+
+class SuiteTestInfo {
+  public:
+    SuiteTestInfo( std::string test_name, SuiteBaseTest *func )
+    {
+        SuiteUmbrella::suites.push_back( Suite{ test_name, func } );
+    }
+};
+
+class SuiteTestName : SuiteBaseTest {
+  public:
+    char *TestBody( ) override;
+
+  private:
+    static SuiteTestInfo *test_info;
+};
+
+SuiteTestInfo *SuiteTestName::test_info =
+    new SuiteTestInfo{ "hello_test_name", new SuiteTestName };
+
+char *
+SuiteTestName::TestBody( )
+{
+    std::cout << "I am SuiteTestName\n";
+    return nullptr;
+}
+
 int
 main( )
 {
-    std::cout << "Hello eve\n";
+    SuiteUmbrella suites{ };
+    suites.RunAllSuites( );
 }
