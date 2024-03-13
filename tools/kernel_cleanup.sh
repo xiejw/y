817e7622fce4d583e06e2ba620ffc52111dc1e7b
xiejw <xiejw.io+dt@gmail.com>
2024-03-13 09:20:58 -0700

    [tools] added kernel cleanup.

---
+++ tools/kernel_cleanup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+if [ -z "${VERSION}" ]; then
+  echo "Please set VERSION like 6.7"
+  exit 1
+fi
+
+echo "========================================================================="
+echo "delete files in /lib/modules"
+echo "========================================================================="
+cd /lib/modules
+ls -l -d *${VERSION}*-xiejw || echo "no files"
+sudo rm -rf *${VERSION}*-xiejw
+
+echo "========================================================================="
+echo "delete files in /boot"
+echo "========================================================================="
+cd /boot
+ls -l -d *${VERSION}*-xiejw || echo "no files"
+sudo rm -rf *${VERSION}*-xiejw
