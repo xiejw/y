f885a7c5a94b35e94e497026e49f00183c8691d1
Jianwei Xie <xiejw0217+db@gmail.com>
2024-05-14 13:43:51 -0700

    [tools] added node modules into fzf ignore list.

--- tools/email.sh
+++ tools/email.sh
@@ -8,13 +8,13 @@
 set -e
 set -x
 
-MOCKINGJAY_HOME=~/Workspace/x/ann/mockingjay
-MOCKINGJAY_CLIENT=${MOCKINGJAY_HOME}/.build/client
+MOCKINGJAY_HOME=${HOME}/Workspace/x/ann/mockingjay
+MOCKINGJAY_CLIENT=${HOME}/Workspace/.bin/email
 
 if ! test -f ${MOCKINGJAY_CLIENT}; then
   echo "mockingjay client not exist"
   echo "building"
-  make -C ${MOCKINGJAY_HOME} release
+  make -C ${MOCKINGJAY_HOME} release install
 fi
 
 ${MOCKINGJAY_CLIENT} "$@"
