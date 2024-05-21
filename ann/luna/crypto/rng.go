1d4342edb5d5a96ab1e9922f66e1fa4ebe355c9b
Jianwei Xie <xiejw0217+db@gmail.com>
2024-05-21 10:55:52 -0700

    [luna] added example code.

--- ann/luna/crypto/rng.go
+++ ann/luna/crypto/rng.go
@@ -3,6 +3,19 @@ package crypo
 
 // Rng represents a family of splittable rng.
 //
+// Example:
+//
+//	r1 := NewRng64(456)
+//	r2 := r1.Split()
+//
+//	size := 4
+//	v1 := make([]uint64, size)
+//	v2 := make([]uint64, size)
+//	for i := 0; i < size; i++ {
+//	    v1[i] = r1.U64()
+//	    v2[i] = r2.U64()
+//	}
+//
 // It also supports serialization and deserialization. The From() call will
 // replace internal state to be identical to the state obtained by To(). The
 // invariance is:
@@ -11,7 +24,7 @@ package crypo
 //	var b: Rng = NewXXX (this might not be initialized)
 //	var b.From(a.To())
 //
-//	if a != b {
+//	if a != b {  // Here `!=`` means struct comparision. Not a valid go Op.
 //	    panic("a should be identical to b")
 //	}
 type Rng interface {
