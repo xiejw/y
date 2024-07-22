57ad0f22628083831f2b990f1efc64d3d53331dd
Jianwei Xie <xiejw0217+db@gmail.com>
2024-07-22 09:07:53 -0700

    [tlpi] handle c_char as both i8 and u8 cases.

--- ann/tlpi/chp62_term_rs/src/main.rs
+++ ann/tlpi/chp62_term_rs/src/main.rs
@@ -1,19 +1,40 @@
 use std::ffi::CStr;
+use std::mem;
+use std::os::raw::c_char;
 
-pub mod console;
+#[path = "console.rs"]
+pub mod console_lib;
 
 fn main() {
-    let mut cle = console::Console::new().unwrap();
-    println!("Type any key.");
+    let mut console = console_lib::Console::new().unwrap();
 
-    cle.run_loop(&mut (), |_, meta| {
-        let cstr = unsafe { CStr::from_ptr(meta.input as *const _) };
+    loop {
+        println!("Type any key.");
+        let mut quit = false;
 
-        println!(
-            "{:?}",
-            String::from_utf8_lossy(cstr.to_bytes()).to_string() //.to_str().unwrap()
-        );
-        console::KeyResponse::Stop
-    })
-    .unwrap();
+        console
+            .run_loop(&mut quit, |quit, meta| {
+                let cstr = unsafe {
+                    // `from_ptr` takes *const c_char which is either i8 or u8 (platform
+                    // depdendent).
+                    CStr::from_ptr(mem::transmute::<_, &[c_char; 256]>(meta.input) as *const _)
+                };
+
+                let response = String::from_utf8_lossy(cstr.to_bytes()).to_string();
+
+                println!("{:?}", response);
+
+                if response == "\u{1b}" {
+                    *quit = true;
+                }
+
+                console_lib::KeyResponse::Stop
+            })
+            .unwrap();
+
+        if quit {
+            println!("Quit");
+            break;
+        }
+    }
 }
