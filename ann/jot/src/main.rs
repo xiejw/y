1957ef68ae9da6bac3cd9b2a552fae134e733ada
xiejw.io+minim2 <xiejw.io+minim2@gmail.com>
2025-03-18 16:41:56 -0700

    [jot] Added anyhow Result.

--- ann/jot/src/main.rs
+++ ann/jot/src/main.rs
@@ -1,11 +1,14 @@
 use std::fs::File;
 use std::io::Read;
 
-fn main() {
+use anyhow::Result;
+
+fn main() -> Result<()> {
     let count = 1000;
 
-    let mut f = File::open("/etc/hosts").unwrap();
+    let mut f = File::open("/etc/hosts")?;
     let mut buf = vec![0; count];
-    f.read(&mut buf).unwrap();
-    print!("{}", String::from_utf8(buf).unwrap());
+    f.read(&mut buf)?;
+    print!("{}", String::from_utf8(buf)?);
+    Ok(())
 }
