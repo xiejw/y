3ccf296f393aa88ee4c1b6049c4e8ce3e981bf80
xiejw.io+m4max@gmail.com <xiejw.io+m4max@gmail.com>
2025-09-19 21:31:33 -0700

    [dotfiles] adjust libcxx to be stable.

--- tools/llvm_compile_libcxx.sh
+++ tools/llvm_compile_libcxx.sh
@@ -5,7 +5,7 @@
 # Bash profile set up
 #
 #     libcxx (){
-#         export LIBCXX_PATH=~/Workspace/build/llvm/install-libcxx
+#         export LIBCXX_PATH=~/Workspace/build/usr/libcxx/
 #         export CXX="clang++ -nostdinc++ -nostdlib++ -isystem ${LIBCXX_PATH}/include/c++/v1 -L ${LIBCXX_PATH}/lib -Wl,-rpath,${LIBCXX_PATH}/lib -lc++"
 #         export CXXFLAGS=-Wno-unused-command-line-argument
 #     }
@@ -24,10 +24,18 @@ set -e
 TODAY_STR=`date '+%Y-%m-%d'`
 LIBCXX_BUILD_DIR=build-libcxx-${TODAY_STR}
 
+LIBCXX_INSTALL_DIR=${HOME}/Workspace/build/usr/libcxx
+LIBCXX_INSTALL_DIR_TMP=${HOME}/Workspace/build/usr/libcxx_${TODAY_STR}
+
 mkdir ${LIBCXX_BUILD_DIR}
 cd ${LIBCXX_BUILD_DIR}
-cmake -G Ninja -S ../src/runtimes -DCMAKE_CXX_COMPILER=clang++ -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"
+cmake -G Ninja -S ../src/runtimes                             \
+    -DCMAKE_CXX_COMPILER=clang++                              \
+    -DCMAKE_INSTALL_PREFIX=${LIBCXX_INSTALL_DIR_TMP}          \
+    -DLLVM_ENABLE_RUNTIMES="libcxx;libcxxabi;libunwind"
 ninja
-cd ..
-ln -sf ${LIBCXX_BUILD_DIR} install-libcxx
+rm -rf ${LIBCXX_INSTALL_DIR}*
+cmake --install .
+ln -sf ${LIBCXX_INSTALL_DIR_TMP} ${LIBCXX_INSTALL_DIR}
+
 
